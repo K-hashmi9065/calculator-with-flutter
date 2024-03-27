d67cfa612aca644a50538ceaf31ceef2b84c5835
k.hashmi9065 <kamranhashmi9065@gmail.com>
2024-03-27 16:02:18 +0530

    some changes

--- lib/views/home_views.dart
+++ lib/views/home_views.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
+import 'calculator_view.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-       child: Container(
-         color: Colors.deepOrange,
-         child: const Center(child: Text('Learn Flutter by Kamran',
-         style: TextStyle(fontSize: 40,
-         color: Colors.white,
-         fontWeight: FontWeight.bold),)),
-       ),
-     );
+    return const Scaffold(
+      backgroundColor: Colors.deepOrange,
+      body: SafeArea(
+        child: CalculatorView(),
+      ),
+
+      // Starting code
+      //  child: Container(
+      //    color: Colors.deepOrange,
+      //    child: const Center(child: Text('Learn Flutter by Kamran',
+      //    style: TextStyle(fontSize: 40,
+      //    color: Colors.white,
+      //    fontWeight: FontWeight.bold),)),
+      //  ),
+    );
   }
 }
